8354e957beec5585a194fd4aa1f1254398080e4a
AJ Collins <16407457+ajc133@users.noreply.github.com>
2024-06-30 13:54:05 -0700

    Update default urls and styling

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path("", views.main, name="main"),
+    path("", RedirectView.as_view(pattern_name="expenses"), name="main"),
     path("users/", views.users, name="users"),
     path("users/details/<int:id>", views.user_details, name="user_details"),
     path("expenses/", views.expenses, name="expenses"),
