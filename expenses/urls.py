40b68f1b7d1fc976c140ab8f162f700feef120bd
AJ Collins <16407457+ajc133@users.noreply.github.com>
2024-06-30 18:17:57 -0700

    fix: Prevent refresh from submitting again
    refactor: Stop using 'id' as function arg, since apparently it's a
    builtin
    
    Fixes #2 and #3

--- expenses/urls.py
+++ expenses/urls.py
@@ -5,8 +5,12 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     path("", RedirectView.as_view(pattern_name="expenses"), name="main"),
     path("users/", views.users, name="users"),
-    path("users/details/<int:id>", views.user_details, name="user_details"),
+    path("users/details/<int:expense_id>", views.user_details, name="user_details"),
     path("expenses/", views.expenses, name="expenses"),
-    path("expenses/<int:id>/details", views.expense_details, name="expense_details"),
-    path("expenses/<int:id>/edit", views.expense_edit, name="expense_edit"),
+    path(
+        "expenses/<int:expense_id>/details",
+        views.expense_details,
+        name="expense_details",
+    ),
+    path("expenses/<int:expense_id>/edit", views.expense_edit, name="expense_edit"),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -9,6 +9,7 @@ from django.http import (
 from django.template import loader, RequestContext
 from django.shortcuts import get_object_or_404
 from django.db.models import Sum
+from django.http.request import QueryDict
 from django.urls import reverse
 from django.views.decorators.http import (
     require_http_methods,
@@ -68,9 +69,9 @@ def expenses(request: HttpRequest):
 
 @require_safe
 @login_required
-def expense_details(request: HttpRequest, id):
+def expense_details(request: HttpRequest, expense_id):
     # TODO: One db query
-    expense = Expense.objects.get(id=id)
+    expense = Expense.objects.get(id=expense_id)
     payer = User.objects.get(id=expense.payer.id)
     submitter_name = User.objects.get(id=expense.submitter.id)
     template = loader.get_template("expense_details.html")
@@ -78,34 +79,42 @@ def expense_details(request: HttpRequest, id):
     return HttpResponse(template.render(context, request))
 
 
+def update_expense(
+    request: HttpRequest, expense_id: int, form: QueryDict
+) -> HttpResponse:
+    submitter = get_object_or_404(User, pk=request.user.id)
+    item = form.get("item")
+    cost = form.get("cost")
+    description = form.get("description")
+    payer_id = form.get("user-id")
+    if item is None or cost is None:
+        return HttpResponseBadRequest()
+
+    Expense.objects.filter(id=expense_id).update(
+        item=item,
+        cost=cost,
+        description=description,
+        payer=payer_id,
+        submitter=submitter,
+        updated_at=datetime.now(tz=timezone.utc),
+    )
+    return HttpResponseRedirect(
+        reverse("expense_details", kwargs={"expense_id": expense_id})
+    )
+
+
 @require_http_methods(["HEAD", "GET", "POST"])
 @login_required
-def expense_edit(request: HttpRequest, id):
-    submitter = get_object_or_404(User, pk=request.user.id)
+def expense_edit(request: HttpRequest, expense_id):
     if request.method == "POST":
-        form = request.POST
-        item = form.get("item")
-        cost = form.get("cost")
-        description = form.get("description")
-        payer_id = form.get("user-id")
-        if item is None or cost is None:
-            return HttpResponseBadRequest()
-        payer = get_object_or_404(User, pk=payer_id)
+        return update_expense(request, expense_id, request.POST)
 
-        Expense.objects.filter(id=id).update(
-            item=item,
-            cost=cost,
-            description=description,
-            payer=payer_id,
-            submitter=submitter,
-            updated_at=datetime.now(tz=timezone.utc),
-        )
     # TODO: One db query
-    expense = Expense.objects.get(id=id)
+    expense = Expense.objects.get(id=expense_id)
     payer = User.objects.get(id=expense.payer.id)
 
     users = list(User.objects.exclude(username="admin").exclude(id=payer.id).all())
-    # We always want submitter to be the first dropdown option
+    # We always want original payer to be the first dropdown option
     users.insert(0, payer)
 
     template = loader.get_template("expense_edit.html")
@@ -124,9 +133,9 @@ def users(request: HttpRequest):
 
 @require_safe
 @login_required
-def user_details(request: HttpRequest, id):
+def user_details(request: HttpRequest, user_id):
     # TODO: One db query
-    user = User.objects.get(id=id)
+    user = User.objects.get(id=user_id)
     user_expenses = user.expense_set.all().values()
     template = loader.get_template("user_details.html")
     context = {"user": user, "expenses": user_expenses}
